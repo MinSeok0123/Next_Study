021877f3e4254671e1bc394ad7923a1667bd3e7b
민석 <80371412+MinSeok0123@users.noreply.github.com>
2022-12-24 23:33:16 +0900

    Fix :: Admin

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "550b98865567c6ba"; }
+/******/ 		__webpack_require__.h = function() { return "6fc8f769883b64f1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,3 +1,9 @@
 export default function admin() {
-  return <div>어드민 페이지</div>;
+  const count = "99";
+  return (
+    <div>
+      <h1>어드민페이지</h1>
+      <li>사용자 수 {count}</li>
+    </div>
+  );
 }
